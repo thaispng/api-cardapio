0172a6bc7add7277310ea33c42da3c230f78d09f
thais ventura <thaisvtr26@gmail.com>
2025-02-24 23:27:28 -0300

    Initial commit

---
+++ test.ts
@@ -0,0 +1,14 @@
+const { PrismaClient } = require('@prisma/client');
+
+const prisma = new PrismaClient();
+
+async function main() {
+    const produtos = await prisma.produto.findMany();
+    console.log(produtos);
+}
+
+main()
+    .catch(e => console.error(e))
+    .finally(async () => {
+        await prisma.$disconnect();
+    });
